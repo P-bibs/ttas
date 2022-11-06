8a9677a0aa4330af0c54d15de6726ae27959792c
Paul Biberstein <biberstein.paul@gmail.com>
2022-11-05 23:45:27 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,154 @@
+use plotters::prelude::*;
+use std::sync::{atomic::AtomicUsize, Arc, Barrier};
+use std::time::{Duration, Instant};
+
+fn main() {
+    let sample_size = 10;
+
+    let thread_counts = vec![10, 25, 50, 100, 200, 400, 800];
+
+    // a lambda to run a locking experiment many times across different thread counts and
+    // accumulates the results
+    let collect_samples = |func: fn(&AtomicUsize, usize)| {
+        thread_counts
+            .iter()
+            .map(|thread_count| {
+                let samples = (0..sample_size)
+                    .map(|_| run_experiment(func, *thread_count))
+                    .collect();
+                println!("Experiment complete for {}", thread_count);
+                (*thread_count, samples)
+            })
+            .collect()
+    };
+
+    let experiments1 = collect_samples(ttas);
+    let experiments2 = collect_samples(tas);
+
+    line_plot(experiments1, experiments2);
+}
+
+/// plots a scatter plot of run duration vs thread count
+fn line_plot(durations1: Vec<(usize, Vec<Duration>)>, durations2: Vec<(usize, Vec<Duration>)>) {
+    // closure to turn raw duration results into data for plotting
+    let make_data = |durations: Vec<(usize, Vec<Duration>)>| {
+        let mut data: Vec<(f64, f64)> = vec![];
+        durations.iter().for_each(|(thread_count, samples)| {
+            samples
+                .iter()
+                .for_each(|sample| data.push((*thread_count as f64, sample.as_nanos() as f64)))
+        });
+        data
+    };
+
+    // turn raw data into plottable data
+    let data1 = make_data(durations1);
+    let data2 = make_data(durations2);
+
+    let out_file_name = "plotters-doc-data/normal-dist.png";
+
+    let root = BitMapBackend::new(out_file_name, (1024*2, 768*2)).into_drawing_area();
+    root.fill(&WHITE).unwrap();
+
+    let areas = root.split_by_breakpoints([944*2], [80*2]);
+
+    let mut scatter_ctx = ChartBuilder::on(&areas[2])
+        .x_label_area_size(80)
+        .y_label_area_size(80)
+        .build_cartesian_2d(0f64..1000f64, (0f64..20_000_000f64).log_scale())
+        .unwrap();
+    scatter_ctx
+        .configure_mesh()
+        .disable_x_mesh()
+        .disable_y_mesh()
+        .draw()
+        .unwrap();
+
+    scatter_ctx
+        .draw_series(
+            data1
+                .iter()
+                .map(|(x, y)| Circle::new((*x, *y), 3, GREEN.stroke_width(1))),
+        )
+        .unwrap();
+    scatter_ctx
+        .draw_series(
+            data2
+                .iter()
+                .map(|(x, y)| Circle::new((*x, *y), 3, RED.stroke_width(1))),
+        )
+        .unwrap();
+
+    // To avoid the IO failure being ignored silently, we manually call the present function
+    root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
+    println!("Result has been saved to {}", out_file_name);
+}
+
+/// runs a lock function on a given number of threads and returns the duration
+fn run_experiment(func: fn(&AtomicUsize, usize), thread_count: usize) -> Duration {
+    let lock = AtomicUsize::new(0);
+    let lock: &'static AtomicUsize = Box::leak(Box::new(lock));
+
+    let barrier = Arc::new(Barrier::new(thread_count + 1));
+
+    let threads: Vec<_> = (0..thread_count)
+        .map(|i| {
+            let barrier = Arc::clone(&barrier);
+            let lock = lock;
+            std::thread::spawn(move || {
+                barrier.wait();
+                func(lock, i);
+            })
+        })
+        .collect();
+
+    barrier.wait();
+
+    let now = Instant::now();
+
+    threads.into_iter().for_each(|t| t.join().unwrap());
+
+    let elapsed = now.elapsed();
+
+    return elapsed;
+}
+
+fn ttas(lock: &AtomicUsize, id: usize) {
+    // acquire lock
+    loop {
+        if lock.load(std::sync::atomic::Ordering::Relaxed) == 0 {
+            if let Ok(_) = lock.compare_exchange(
+                0,
+                id,
+                std::sync::atomic::Ordering::SeqCst,
+                std::sync::atomic::Ordering::SeqCst,
+            ) {
+                break;
+            }
+        }
+    }
+
+    // ... do work
+
+    // release lock
+    lock.store(0, std::sync::atomic::Ordering::SeqCst);
+}
+
+fn tas(lock: &AtomicUsize, id: usize) {
+    // acquire lock
+    loop {
+        if let Ok(_) = lock.compare_exchange(
+            0,
+            id,
+            std::sync::atomic::Ordering::SeqCst,
+            std::sync::atomic::Ordering::SeqCst,
+        ) {
+            break;
+        }
+    }
+
+    // ... do work
+
+    // release lock
+    lock.store(0, std::sync::atomic::Ordering::SeqCst);
+}
